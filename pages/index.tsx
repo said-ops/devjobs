489a246ebe608aff5af8508232c0fd9e3f5277ec
said-ops <idrissisaid29@gmail.com>
2025-03-03 22:32:40 +0000

    adding static header and search bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 function index() {
   return (
-    <div className='bg-green-700 font-kumbh text-xl m-auto'>index</div>
+    <Header/>
   )
 }
 
