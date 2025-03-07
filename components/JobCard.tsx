4a5bb852ef5080ae0357d0072f8aa42dc989e413
said-ops <idrissisaid29@gmail.com>
2025-03-07 12:49:02 +0000

    responsive grid layout to display job cards

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -3,8 +3,8 @@ import React from "react";
 function JobCard() {
   return (
     // job card
-    <div className="flex flex-col justify-center gap-6 p-4 rounded-md transform transition-all ease-in-out  font-kumbh bg-white w-80 h-56">
-        {/* logo */}
+    <div className="flex flex-col justify-center gap-6 p-4 rounded-md transform transition-all ease-in-out  font-kumbh bg-white w-[327px] md:w-[339px] lg:w-[350px] h-56 m-auto">
+      {/* logo */}
       <div className="w-[50px] h-[50px] bg-[hsl(36,87%,49%)] flex items-center justify-center rounded-xl absolute top-[-1.5rem] left-[1.5rem]">
         <img src="/assets/logos/scoot.svg" alt="company logo" />
       </div>
@@ -12,12 +12,14 @@ function JobCard() {
       <div className="mt-4 flex flex-col gap-2">
         {/* date posted */}
         <div className="font-light text-gray flex items-center gap-1">
-        <span>5h ago</span>
-        <span className="font-bold">.</span>
-        <span>full time</span>
+          <span>5h ago</span>
+          <span className="font-bold">.</span>
+          <span>full time</span>
         </div>
         {/* job title */}
-        <h2 className="text-veryDarkBlue text-[20px] font-bold">Senior Software Engineer</h2>
+        <h2 className="text-veryDarkBlue text-[20px] font-bold">
+          Senior Software Engineer
+        </h2>
         {/* company name */}
         <span className="text-gray font-light">scoot</span>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/common/Button'
 import Header from '@/components/Header'
 import JobCard from '@/components/JobCard'
 import React from 'react'
@@ -6,8 +7,17 @@ function index() {
   return (
     <>
       <Header/>
-      <main className='trasnform translate-y-12'>
+      <main className='trasnform translate-y-12 py-12 px-4 m-auto grid gap-y-16 lg:gap-x-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:max-w-[1110px]'>
         <JobCard/>
+        <JobCard/>
+        <JobCard/>
+        <JobCard/>
+        <JobCard/>
+        <JobCard/>
+        <JobCard/>
+        <JobCard/>
+        <JobCard/>
+        {/* <Button /> */}
       </main>
     </>
   )
