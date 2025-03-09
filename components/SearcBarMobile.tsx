314d6b1d87ee4a38fd778178afab1dddbbc17294
said-ops <idrissisaid29@gmail.com>
2025-03-09 14:47:48 +0000

    deploy

--- components/SearcBarMobile.tsx
+++ components/SearcBarMobile.tsx
@@ -12,7 +12,7 @@ function SearcBarMobile() {
   const setLocation = useJobStore((state) => state.setLocation);
   const filterJobs = useJobStore((state) => state.filterJobs);
 
-  const handleSearch = (e:any) => {
+  const handleSearch = (e:React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     filterJobs(title, location, isFullTime);
     setOpen(!open)
